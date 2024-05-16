b379818757b95ffacf231908c88469893b930572
Suprit Dagade <supritdagade77@gmail.com>
2024-05-16 17:37:13 +0530

    google verification meta tag added to master page

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -12,7 +12,7 @@
     <meta name="keywords" content="@yield('keyword')">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <!-- verification code  -->
-    <meta name="google-site-verification" content="_7En7MJwLW__XKlHyMbAIhIH_rLnHc--c-QMKDe1kBQ" />
+    <meta name="google-site-verification" content="siSA-XIq0trjG29MdsE2rYrVbGgFP9XtrsyNzmwxfXY" />
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-1Y7WGKJ5KK"></script>
     <script>
