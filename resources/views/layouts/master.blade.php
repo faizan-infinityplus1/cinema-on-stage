a202e1fdeaf1d47cbbb04e774aec71fd0ebd9ff6
Suprit Dagade <supritdagade77@gmail.com>
2024-05-21 17:16:49 +0530

    new pages added to project

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -682,16 +682,14 @@
                                 <ul>
 
                                     <li class="has-dropdown" id="home_mobile">
-                                        <a href="{{ route('home') }}" class="button upper_menu_margin1"
-                                            id="button-7">
+                                        <a href="{{ route('home') }}" class="button upper_menu_margin1" id="button-7">
                                             <div id="dub-arrow">Home</div>
                                             <button href="{{ route('home') }}">Home</button>
                                         </a>
                                     </li>
 
                                     <li class="active has-dropdown">
-                                        <a href="{{ route('about') }}" class="button upper_menu_margin1"
-                                            id="button-7">
+                                        <a href="{{ route('about') }}" class="button upper_menu_margin1" id="button-7">
                                             <div id="dub-arrow">About Us</div>
                                             <button href="{{ route('about') }}">About Us</button>
                                         </a>
@@ -703,8 +701,7 @@
                                             <div id="dub-arrow">About show</div>
                                             <button href="{{ route('about.show') }}">About Show </button>
                                         </a>
-
-
+                                    </li>
                                     <li class="has-dropdown ">
                                         <a href="{{ route('book.now') }}" class="button upper_menu_margin1"
                                             id="button-7">
@@ -714,7 +711,45 @@
                                         <!-- <div id="dub-arrow">Home</div>
                                     <a href="tour-schedule.php"> Book Now </a> -->
 
+                                        <div class="nav flex-column  nav-pills me-3" id="v-pills-tab"
+                                            role="tablist" aria-orientation="vertical">
+                                            <button class="nav-link   nav-tic active" id="v-pills-home-tab"
+                                                data-bs-toggle="pill" data-bs-target="#v-pills-home" type="button"
+                                                role="tab" aria-controls="v-pills-home" aria-selected="true"
+                                                style=" padding:15px; color: #df4f06; background-color:white; border:2px solid #df4f06; font-size:1.6rem; font-weight:600;"><a href="{{ route('houston') }}">Houston</a></button>
+                                            <button class="nav-link  nav-tic new_jersey" id="v-pills-profile-tab"
+                                                data-bs-toggle="pill" data-bs-target="#v-pills-profile" type="button"
+                                                role="tab" aria-controls="v-pills-profile" aria-selected="false"
+                                                style=" padding:15px; margin-top: 20px; color: #df4f06; background-color:white; border:2px solid #df4f06; font-size:1.6rem; font-weight:600;">
+                                            New Jersey
+                                            </button>
+                                            <button class="nav-link nav-tic new_york " id="v-pills-messages-tab"
+                                                data-bs-toggle="pill" data-bs-target="#v-pills-messages" type="button"
+                                                role="tab" aria-controls="v-pills-messages" aria-selected="false"
+                                                style=" padding:15px; margin-top: 20px; color: #df4f06; background-color:white; border:2px solid #df4f06; font-size:1.6rem; font-weight:600;"><a href="{{ route('new.york') }}">New York</a></button>
+                                            <button class="nav-link nav-tic dallas " id="v-pills-settings-tab"
+                                                data-bs-toggle="pill" data-bs-target="#v-pills-settings" type="button"
+                                                role="tab" aria-controls="v-pills-settings" aria-selected="false"
+                                                style=" padding:15px; margin-top: 20px; color: #df4f06; background-color:white; border:2px solid #df4f06; font-size:1.6rem; font-weight:600;"><a href="{{ route('dallas') }}">Dallas</a></button>
+                                            <button class="nav-link nav-tic san_jose " id="v-pills-la-tab"
+                                                data-bs-toggle="pill" data-bs-target="#v-pills-la" type="button"
+                                                role="tab" aria-controls="v-pills-settings" aria-selected="false"
+                                                style=" padding:15px; margin-top: 20px; color: #df4f06; background-color:white; border:2px solid #df4f06; font-size:1.6rem; font-weight:600;">San
+                                                Jose</button>
+                                            <button class="nav-link nav-tic l_a " id="v-pills-new-tab"
+                                                data-bs-toggle="pill" data-bs-target="#v-pills-new" type="button"
+                                                role="tab" aria-controls="v-pills-settings" aria-selected="false"
+                                                style=" padding:15px; margin-top: 20px; color: #df4f06; background-color:white; border:2px solid #df4f06; font-size:1.6rem; font-weight:600;">LA</button>
+
+                                        </div>
+                                        
                                     </li>
+                                    <li class="has-dropdown mobile-menu" >Houston</li>
+                                    <li class="has-dropdown mobile-menu" >New Jersey</li>
+                                    <li class="has-dropdown mobile-menu" >New York</li>
+                                    <li class="has-dropdown mobile-menu" >Dallas</li>
+                                    <li class="has-dropdown mobile-menu" >San Jose</li>
+                                    <li class="has-dropdown mobile-menu" >LA</li>
                                     <li class="has-dropdown">
                                         <a href="{{ route('past.shows') }}" class="button upper_menu_margin1"
                                             id="button-7">
@@ -724,8 +759,7 @@
                                     </li>
                                     <!-- abdul edited -->
                                     <li class="has-dropdown">
-                                        <a href="{{ route('blog') }}" class="button upper_menu_margin1"
-                                            id="button-7">
+                                        <a href="{{ route('blog') }}" class="button upper_menu_margin1" id="button-7">
                                             <div id="dub-arrow">Blog</div>
                                             <button href="{{ route('blog') }}">Blog</button>
                                         </a>
@@ -759,8 +793,16 @@
                                     </li>
                                     <li class="has-dropdown">
                                         <a href="{{ route('about.show') }}">About show</a>
-
+                                        
                                     </li>
+                                    
+                                    <li class="has-dropdown" id="button-7">Houston</li>
+                                    <li class="has-dropdown" id="button-7">New Jersey</li>
+                                    <li class="has-dropdown" id="button-7">New York</li>
+                                    <li class="has-dropdown" id="button-7">Dallas</li>
+                                    <li class="has-dropdown" id="button-7">San Jose</li>
+                                    <li class="has-dropdown" id="button-7">LA</li>
+                                
                                     <li class="has-dropdown">
                                         <a href="tour-schedule.php"> Tour Schedule </a>
 
@@ -799,8 +841,7 @@
             <div class="fm-header-sidebar-wrapper">
                 <div class="fm-header-sidebar-action mb-75">
                     <div class="fm-header-sidebar-logo">
-                        <a href="#"><img src="{{ asset('assets/img/logo (1).png') }}"
-                                alt="img not found" /></a>
+                        <a href="#"><img src="{{ asset('assets/img/logo (1).png') }}" alt="img not found" /></a>
                     </div>
                     <div class="offcanvas-close">
                         <a class="offcanvas-btn" href="#0">
@@ -861,8 +902,8 @@
                     <div class="fm-offcanvas-social">
                         <span class="social-text">Follow :</span>
                         <div class="fm-offcanvas-social-icon" style="color: black;">
-                            <a href="https://www.facebook.com/MughalEAzamPlay/" target="_blank"
-                                style="color: black;"><i class="fa-brands fa-facebook-f"></i></a>
+                            <a href="https://www.facebook.com/MughalEAzamPlay/" target="_blank" style="color: black;"><i
+                                    class="fa-brands fa-facebook-f"></i></a>
                             <a href="https://twitter.com/mughaleazamplay" target="_blank"><i
                                     class="fa-brands fa-twitter"></i></a>
                             <a href="https://www.instagram.com/mughaleazamplay/" target="_blank"><i
@@ -953,8 +994,7 @@
                         </div>
                         <ul class="wow zoomInLeft">
                             <li><a href="{{ route('home') }}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                        viewBox="0 0 24 24">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                                         <path fill="none" stroke="#e04b00" stroke-width="2"
                                             d="M5 9h7V5.483a.2.2 0 0 1 .341-.142L19 12l-6.659 6.659a.2.2 0 0 1-.341-.142V15H5">
                                         </path>
@@ -962,8 +1002,7 @@
                                     <span style="color:white">Home</span>
                                 </a></li>
                             <li><a href="{{ route('about') }}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                        viewBox="0 0 24 24">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                                         <path fill="none" stroke="#e04b00" stroke-width="2"
                                             d="M5 9h7V5.483a.2.2 0 0 1 .341-.142L19 12l-6.659 6.659a.2.2 0 0 1-.341-.142V15H5">
                                         </path>
@@ -972,8 +1011,7 @@
                                 </a></li>
                             <!-- .php remove kiya bas -->
                             <li><a href="{{ route('about.show') }}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                        viewBox="0 0 24 24">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                                         <path fill="none" stroke="#e04b00" stroke-width="2"
                                             d="M5 9h7V5.483a.2.2 0 0 1 .341-.142L19 12l-6.659 6.659a.2.2 0 0 1-.341-.142V15H5">
                                         </path>
@@ -981,8 +1019,7 @@
                                     <span style="color:white">About Show</span>
                                 </a></li>
                             <li><a href="{{ route('book.now') }}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                        viewBox="0 0 24 24">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                                         <path fill="none" stroke="#e04b00" stroke-width="2"
                                             d="M5 9h7V5.483a.2.2 0 0 1 .341-.142L19 12l-6.659 6.659a.2.2 0 0 1-.341-.142V15H5">
                                         </path>
@@ -990,8 +1027,7 @@
                                     <span style="color:white">Book Now</span>
                                 </a></li>
                             <li><a href="{{ route('blog') }}">
-                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
-                                        viewBox="0 0 24 24">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
                                         <path fill="none" stroke="#e04b00" stroke-width="2"
                                             d="M5 9h7V5.483a.2.2 0 0 1 .341-.142L19 12l-6.659 6.659a.2.2 0 0 1-.341-.142V15H5">
                                         </path>
@@ -1086,7 +1122,7 @@
 
 
     <script>
-        $(document).ready(function() {
+        $(document).ready(function () {
             $(".fancybox").fancybox({
                 'width': 1000,
                 fitToView: true,
@@ -1158,8 +1194,8 @@
             ];
 
             // Loop through each tab ID
-            tabIDs.forEach(function(tabID) {
-                $(tabID).on('click', function() {
+            tabIDs.forEach(function (tabID) {
+                $(tabID).on('click', function () {
                     // Find the element with both id "dynamictabstrp" and class "active"
                     var $target = $(".tab-pane.active");
 
