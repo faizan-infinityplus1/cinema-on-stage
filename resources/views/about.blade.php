2def3af703f6a661cdee528446fd20d65eeb3c4e
Suprit Dagade <supritdagade77@gmail.com>
2024-05-30 13:43:23 +0530

    about page formated

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -6,80 +6,92 @@
 @section('keyword', 'Timesquare Mughal E Azam, Mughal E Azam Timesquare, mughal e azam show tickets')
 @section('content')
 
-  <!-- <div class="adjust-header-space bg-common-black"></div> -->
+<!-- <div class="adjust-header-space bg-common-black"></div> -->
 
-      <!-- breadcrumb-area-start -->
-      <div class="fm-breadcrumb-area" data-background="assets/img/banner9.jpg">
+<!-- breadcrumb-area-start -->
+<div class="fm-breadcrumb-area" data-background="assets/img/banner9.jpg">
 
-         <div class="container">
-            <div class="row justify-content-center">
-               <div class="col-12">
-                  <div class="page-title-wrapper">
-                     <h1 class="page-title p-text">About Us</h1>
-                  </div>
-                  <div class="breadcrumb-menu">
-                     <nav aria-label="Breadcrumbs" class="breadcrumb-trail breadcrumbs">
-                        <ul class="trail-items">
-                           <li class="trail-item trail-begin p-text">
-                              <a href="index.html"><span>Home</span></a>
-                           </li>
-                           <li class="trail-item trail-end p-text"><span>About Us</span></li>
-                        </ul>
-                     </nav>
-                  </div>
+   <div class="container">
+      <div class="row justify-content-center">
+         <div class="col-12">
+            <div class="page-title-wrapper">
+               <h1 class="page-title p-text">About Us</h1>
+            </div>
+            <div class="breadcrumb-menu">
+               <nav aria-label="Breadcrumbs" class="breadcrumb-trail breadcrumbs">
+                  <ul class="trail-items">
+                     <li class="trail-item trail-begin p-text">
+                        <a href="index.html"><span>Home</span></a>
+                     </li>
+                     <li class="trail-item trail-end p-text"><span>About Us</span></li>
+                  </ul>
+               </nav>
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+<!-- breadcrumb-area-end -->
+
+
+<!--  -->
+<div class="about-show text-center mt-3 lg-p-4 md-p-0 sm-p-0 " style="padding-bottom:50px">
+   <div class="container">
+      <div class="row">
+         <h1 class="fade__anim-item ibm-plex " style="color: #fd6500;margin-top:50px">About Cinema On Stage</h1>
+         <p class="title-anim1 monstarat about_first_para" style="color:#6e777d; margin-top:20px">Welcome to Cinema On
+            Stage, your premier entertainment company based in Houston, North America, with a global footprint spanning
+            over 30 countries and 70 cities. With a rich legacy of delivering exceptional performances on a global
+            scale, we bring unparalleled expertise and experience to every production.</p>
+
+
+         <div class="col-xl-6 col-lg-6 ">
+
+
+            <div class=" fade_animation text-start fade__anim-item  ">
+               <p class="title-anim1 monstarat" style="color:#6e777d; margin-top:20px" class=" pt-20">From breathtaking
+                  theatrical experiences to electrifying concerts and meticulously orchestrated corporate events, each
+                  production is meticulously designed to captivate and inspire audiences of all ages. Our team of
+                  technical experts and production managers ensures that every moment on stage is nothing short of
+                  extraordinary, employing cutting-edge technology and innovative staging techniques to create immersive
+                  experiences that leave a lasting impression.
+               </p>
+               <p class="title-anim1 monstarat" style="color:#6e777d; margin-top:20px" class="pt-20">As a top Bollywood
+                  entertainment company, we pride ourselves on our ability to seamlessly blend artistry, storytelling,
+                  and entertainment, ensuring that each show resonates with audiences worldwide. Our commitment to
+                  excellence has earned us a reputation as one of the top event companies in Houston and beyond, with a
+                  track record of delivering unforgettable experiences that exceed expectations.
+               </p>
+               <p class="title-anim1 monstarat about_last_para" style="color:#6e777d; margin-top:20px" class="pt-20">
+                  Whether you're seeking a thrilling Bollywood show, expert technical execution, or seamless event
+                  management, Cinema On Stage is your trusted partner for unforgettable entertainment experiences. Join
+                  us as we continue to push the boundaries of creativity and production quality, delivering
+                  extraordinary shows that captivate hearts and minds around the world.
+               </p>
+            </div>
+
+         </div>
+         <div class="col-xl-6 col-lg-6" style="align-items: center; display: flex;">
+            <div class="fm-achievement-thumb-wrapper ">
+               <div class="fm-achievement-thumb">
+                  <img src="assets/img/our-ethos.jpg" alt="achievement-thumb">
+               </div>
+               <div class="fm-achievement-thumb">
+                  <img src="assets/img/8.jpeg" alt="achievement-thumb">
+               </div>
+               <div class="fm-achievement-thumb">
+                  <img src="assets/img/our-ethos1.jpg" alt="achievement-thumb">
                </div>
             </div>
          </div>
       </div>
-      <!-- breadcrumb-area-end -->
+   </div>
+</div>
 
+<!--  -->
 
-      <!--  -->
-      <div class="about-show text-center mt-3 lg-p-4 md-p-0 sm-p-0 " style="padding-bottom:50px">
-         <div class="container">
-            <div class="row">
-               <h1 class="fade__anim-item ibm-plex " style="color: #fd6500;margin-top:50px">About Cinema On Stage</h1>
-               <p class="title-anim1 monstarat about_first_para" style="color:#6e777d; margin-top:20px">Welcome to Cinema On Stage, your premier entertainment company based in Houston, North America, with a global footprint spanning over 30 countries and 70 cities. With a rich legacy of delivering exceptional performances on a global scale, we bring unparalleled expertise and experience to every production.</p>
+<div class="container">
+   <div class="hr1"></div>
+</div>
 
-
-               <div class="col-xl-6 col-lg-6 ">
-
-
-                  <div class=" fade_animation text-start fade__anim-item  ">
-                     <p class="title-anim1 monstarat" style="color:#6e777d; margin-top:20px" class=" pt-20">From breathtaking theatrical experiences to electrifying concerts and meticulously orchestrated corporate events, each production is meticulously designed to captivate and inspire audiences of all ages. Our team of technical experts and production managers ensures that every moment on stage is nothing short of extraordinary, employing cutting-edge technology and innovative staging techniques to create immersive experiences that leave a lasting impression.
-                     </p>
-                     <p class="title-anim1 monstarat" style="color:#6e777d; margin-top:20px" class="pt-20">As a top Bollywood entertainment company, we pride ourselves on our ability to seamlessly blend artistry, storytelling, and entertainment, ensuring that each show resonates with audiences worldwide. Our commitment to excellence has earned us a reputation as one of the top event companies in Houston and beyond, with a track record of delivering unforgettable experiences that exceed expectations.
-                     </p>
-                     <p class="title-anim1 monstarat about_last_para" style="color:#6e777d; margin-top:20px" class="pt-20">Whether you're seeking a thrilling Bollywood show, expert technical execution, or seamless event management, Cinema On Stage is your trusted partner for unforgettable entertainment experiences. Join us as we continue to push the boundaries of creativity and production quality, delivering extraordinary shows that captivate hearts and minds around the world.
-                     </p>
-
-                  </div>
-
-               </div>
-               <div class="col-xl-6 col-lg-6" style="align-items: center;
-    display: flex;">
-                  <div class="fm-achievement-thumb-wrapper ">
-                     <div class="fm-achievement-thumb">
-                        <img src="assets/img/our-ethos.jpg" alt="achievement-thumb">
-                     </div>
-                     <div class="fm-achievement-thumb">
-                        <img src="assets/img/8.jpeg" alt="achievement-thumb">
-
-                     </div>
-                     <div class="fm-achievement-thumb">
-                        <img src="assets/img/our-ethos1.jpg" alt="achievement-thumb">
-
-                     </div>
-                  </div>
-               </div>
-            </div>
-         </div>
-      </div>
-
-      <!--  -->
-
-      <div class="container">
-         <div class="hr1"></div>
-      </div>
-      
 @endsection
