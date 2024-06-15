a688bd3be841b3e01814b7d881dbfcbbddfbeb53
Suprit Dagade <supritdagade77@gmail.com>
2024-06-15 18:53:22 +0530

    table create for qr users

---
+++ database/migrations/2024_06_15_131414_qr_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        //
+        Schema::create('qr_users', function (Blueprint $table) {
+            $table->id();
+            $table->string('country');
+            $table->string('state');
+            $table->string('city');
+            $table->timestamp('created_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+};
